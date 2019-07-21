65958cc7023c6505d5ae0f37933ce745026e6844
KattyKing <38794317+KattyKing@users.noreply.github.com>
2019-07-21 17:51:20 +0200

    Create Cube.py

---
+++ Cube/Cube.py
@@ -0,0 +1,38 @@
+from machine import Pin
+from time import sleep
+from machine import Pin
+from neopixel import NeoPixel
+import urandom
+
+
+button_pin = Pin(5, Pin.IN)
+strip_pin = Pin(2, Pin.OUT)
+
+count_of_LED = 8
+np = NeoPixel(strip_pin, count_of_LED)
+
+def button_pressed():
+	led_range = urandom.getrandbits(3) + 1
+	for l_r in range(0, led_range):
+		np[l_r] = (255, 255, 255)
+	for l_r in range(led_range + 1, count_of_LED):
+		np[l_r] = (0, 0, 0)
+	np.write()
+	sleep(1)
+
+	
+def button_released():
+	for x in range(0, 8):
+		np[x] = (0, 0, 0)
+	np.write()
+
+
+def cube():
+	while True:
+		if button_pin.value() == 0:
+			button_pressed()
+		elif button_pin.value() == 1:
+			button_released()
+
+			
+cube()
