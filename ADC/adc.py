a1b5b02861addc6358695b05ae02ddb7266e58a2
KattyKing <38794317+KattyKing@users.noreply.github.com>
2019-08-01 20:26:15 +0200

    Update adc.py

--- ADC/adc.py
+++ ADC/adc.py
@@ -1,1 +1,28 @@
-xx
+from machine import Pin, ADC
+from time import sleep
+
+LDR = ADC(0)
+pin_diode = Pin(4, Pin.OUT)
+pin_second_diode = Pin(2, Pin.OUT)
+
+def read_value():
+	pot_value = LDR.read()
+	return pot_value	
+	
+def blink(pot_value):
+	if pot_value < 900:
+		pin_diode.value(1)
+		pin_second_diode.value(0)
+	
+	else:
+		pin_diode.value(0)
+		pin_second_diode.value(1)
+	
+def loop():
+	while True:
+		ldr_value = read_value()
+		blink(ldr_value)
+		sleep(0.1)
+		
+loop()
+		
